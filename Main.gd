d53a463cfb6a4fc2be30d022c447876d80a2cfec
Eugenio <euge.mendoz@gmail.com>
2020-06-20 20:17:00 +0100

    New BG music

--- Main.gd
+++ Main.gd
@@ -3,14 +3,16 @@ extends Node
 onready var startButton = $UI/Buttons/StartButton
 onready var continueButton = $UI/Buttons/ContinueButton
 onready var sfxMove = $SFXMove
+onready var bgMusic = $BGMusic
 onready var animationPlayer = $AnimationPlayer
-
+onready var tween = $Tween
 func _ready():
 	startButton.connect("pressed", self, "on_startButton_pressed")
 	continueButton.connect("pressed", self, "on_continueButton_pressed")
 	SaveFile._load()
 	if SaveFile.save_state:
 		continueButton.visible = true
+
 
 func on_startButton_pressed():
 	SaveFile.reset()
@@ -20,7 +22,13 @@ func on_continueButton_pressed():
 	go_to_battle()
 
 func go_to_battle():
+	fade_out()
 	sfxMove.play()
 	animationPlayer.play("FadeIn")
 	yield(animationPlayer, "animation_finished")
+	bgMusic.stop()
 	var _result = get_tree().change_scene("res://Battle.tscn")
+
+func fade_out():
+	tween.interpolate_property(bgMusic, "volume_db", 0, -80, 1, Tween.TRANS_SINE, Tween.EASE_IN)
+	tween.start()
