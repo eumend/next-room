7f48845dce6e1e41da17e8d23b79560c7abd6c7b
Eugenio <euge.mendoz@gmail.com>
2020-06-21 18:43:11 +0100

    Credits + tweaks

--- Main.gd
+++ Main.gd
@@ -1,18 +1,26 @@
 extends Node
-
+onready var buttonsBox = $UI/Buttons
+onready var creditsBox = $UI/CreditsContainer
 onready var startButton = $UI/Buttons/StartButton
 onready var continueButton = $UI/Buttons/ContinueButton
+onready var creditsButton = $UI/Buttons/CreditsButton
 onready var sfxMove = $SFXMove
 onready var bgMusic = $BGMusic
 onready var animationPlayer = $AnimationPlayer
 onready var tween = $Tween
+onready var backFromCreditsButton = $UI/CreditsContainer/HBoxContainer/Back
 func _ready():
 	startButton.connect("pressed", self, "on_startButton_pressed")
 	continueButton.connect("pressed", self, "on_continueButton_pressed")
+	creditsButton.connect("pressed", self, "on_creditsButton_pressed")
+	backFromCreditsButton.connect("pressed", self, "on_backFromCreditsButton_pressed")
 	SaveFile._load()
 	if SaveFile.save_state:
 		continueButton.visible = true
 
+func on_backFromCreditsButton_pressed():
+	creditsBox.hide()
+	buttonsBox.show()
 
 func on_startButton_pressed():
 	SaveFile.reset()
@@ -20,6 +28,10 @@ func on_startButton_pressed():
 
 func on_continueButton_pressed():
 	go_to_battle()
+
+func on_creditsButton_pressed():
+	buttonsBox.hide()
+	creditsBox.show()
 
 func go_to_battle():
 	fade_out_bg_music()
