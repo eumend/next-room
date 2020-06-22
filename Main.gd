b3dd759d69a8a4458346887331885d41693be5f3
Eugenio <euge.mendoz@gmail.com>
2020-06-22 10:00:31 +0100

    Extend credits

--- Main.gd
+++ Main.gd
@@ -1,6 +1,7 @@
 extends Node
 onready var buttonsBox = $UI/Buttons
-onready var creditsBox = $UI/CreditsContainer
+onready var creditsBox1 = $UI/CreditsContainer/C1
+onready var creditsBox2 = $UI/CreditsContainer/C2
 onready var startButton = $UI/Buttons/StartButton
 onready var continueButton = $UI/Buttons/ContinueButton
 onready var creditsButton = $UI/Buttons/CreditsButton
@@ -8,19 +9,26 @@ onready var sfxMove = $SFXMove
 onready var bgMusic = $BGMusic
 onready var animationPlayer = $AnimationPlayer
 onready var tween = $Tween
-onready var backFromCreditsButton = $UI/CreditsContainer/HBoxContainer/Back
+onready var backFromCreditsButton = $UI/CreditsContainer/C2/Back
+onready var creditsNextButton = $UI/CreditsContainer/C1/Next
+
 func _ready():
 	startButton.connect("pressed", self, "on_startButton_pressed")
 	continueButton.connect("pressed", self, "on_continueButton_pressed")
 	creditsButton.connect("pressed", self, "on_creditsButton_pressed")
 	backFromCreditsButton.connect("pressed", self, "on_backFromCreditsButton_pressed")
+	creditsNextButton.connect("pressed", self, "on_creditsNextButton_pressed")
 	SaveFile._load()
 	if SaveFile.save_state:
 		continueButton.visible = true
 
 func on_backFromCreditsButton_pressed():
-	creditsBox.hide()
+	creditsBox2.hide()
 	buttonsBox.show()
+
+func on_creditsNextButton_pressed():
+	creditsBox1.hide()
+	creditsBox2.show()
 
 func on_startButton_pressed():
 	SaveFile.reset()
@@ -31,7 +39,7 @@ func on_continueButton_pressed():
 
 func on_creditsButton_pressed():
 	buttonsBox.hide()
-	creditsBox.show()
+	creditsBox1.show()
 
 func go_to_battle():
 	fade_out_bg_music()
